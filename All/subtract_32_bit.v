82f885ab815ebf96600e99b94f592c3173cc3407
Piyarach <piyaracht59@email.nu.ac.th>
2016-11-20 17:27:29 +0700

    Update subtract_32_bit.v

--- All/subtract_32_bit.v
+++ All/subtract_32_bit.v
@@ -5,10 +5,11 @@ module inverter(i,o);
 endmodule
 
 module subtract_32_bit(a,b,sum,cout);
-	input [31:0] a, b;
-	output [31:0] sum;
-	output cout,cout2;
-	wire [31:0] carry,temp_invert,temp_add,temp_sum;
+	input signed [31:0] a, b;
+	output signed [31:0] sum;
+	output cout;
+	wire [31:0] temp_invert;
+	wire cin;
 	genvar i;
 	
 	// invert b and store to temp_invert
@@ -17,9 +18,7 @@ module subtract_32_bit(a,b,sum,cout);
 			inverter inv(b[i],temp_invert[i]);
 	end endgenerate
 	
-	// 2 complement
-	assign temp_add = 1;adder_32_bit add1(temp_invert,temp_add,temp_sum,cout2);
-	
+	assign cin = 1'b1;
 	// add a and b 
-	adder_32_bit add2(a,temp_sum,sum,cout);
+	adder_32_bit add2(a,temp_invert,cin,sum,cout);
 endmodule
