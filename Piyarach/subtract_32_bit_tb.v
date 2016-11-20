c212a1027ee519a6c4a80884ad0aa17e25949fd4
Piyarach <piyaracht59@email.nu.ac.th>
2016-11-20 17:24:52 +0700

    Update subtract_32_bit_tb.v

--- Piyarach/subtract_32_bit_tb.v
+++ Piyarach/subtract_32_bit_tb.v
@@ -1,8 +1,8 @@
 module subtract_32_bit_tb;
 	
-	reg[31:0] a , b;
+	reg signed [31:0] a , b;
 	reg cin;
-	wire[31:0] sum;
+	wire signed [31:0] sum;
 	wire cout;
 	
 	subtract_32_bit s1(a,b,sum,cout);
@@ -10,8 +10,11 @@ module subtract_32_bit_tb;
 		$monitor(a, ,b, , ,sum, ,cout);
 		a = 0; b = 0;
 		#1 a = 7; b = 6;
+		#1 a = -7; b = 6;
 		#1 a = 32'b00000000000000000000000000100000; b = 32'b00000000000000000000000000010000;
 		#1 a = 128; b = 64;
+		#1 a = 6; b = 7;
 		#1 $finish;
 	end
 endmodule
+
