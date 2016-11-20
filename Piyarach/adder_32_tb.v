e177d1a7084e05be127afe627ad309007314569e
Piyarach <piyaracht59@email.nu.ac.th>
2016-11-20 17:25:35 +0700

    Update adder_32_tb.v

--- Piyarach/adder_32_tb.v
+++ Piyarach/adder_32_tb.v
@@ -5,13 +5,13 @@ module adder_32_bit_tb;
 	wire[31:0] sum;
 	wire cout;
 	
-	adder_32_bit a1(a,b,sum,cout);
+	adder_32_bit a1(a,b,cin,sum,cout);
 	initial begin
 		$monitor(a, ,b, , ,sum, ,cout);
-		a = 0; b = 0;
-		#1 a = 5; b = 9;
-		#1 a = 32'b00000000000000000000000000010000; b = 32'b00000000000000000000000000100000;
-		#1 a = 128; b = 64;
+		a = 0; b = 0; cin = 0;
+		#1 a = 5; b = 9; cin = 1'b0;
+		#1 a = 32'b00000000000000000000000000010000; b = 32'b00000000000000000000000000100000; cin = 1'b0;
+		#1 a = 128; b = 64; cin = 1'b0;
 		#1 $finish;
 	end
 endmodule
